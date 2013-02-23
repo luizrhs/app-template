4e1cadbe1ec484959db28a1de36576ea363ef26a
Paulo Vasconcellos <luiz.rhs@gmail.com>
2013-02-23 01:29:27 -0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.initConfig({
+
+		// LESS
+		less: {
+			development: {
+				files: {
+					"app/css/global.css": "app/css/*.less"
+				}
+			}
+		},
+
+		// JSHINT
+		jshint: {
+			files: ['app/js/*.js', 'app/js/**/*.js', 'Gruntfile.js',
+			'test/*.js', 'test/**/*.js']
+		},
+
+		watch: {
+			files: ['<%= jshint.files %>', 'app/css/*.less'],
+			tasks: ['jshint', 'concat', 'less']
+		},
+
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					base: 'app',
+					keepalive: true
+				}
+			}
+		},
+
+		concat: {
+			options: {
+				separator: ";"
+			},
+			dist: {
+				src: ['app/js/*.js', 'app/js/**/*.js'],
+				dest: 'app/.dist-js/main.js'
+			}
+		}
+
+	});
+
+};
---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+var a = 10;
+a = 20;
+
+var arg = "arg";
+var go = "asd";
+var k = 10;
